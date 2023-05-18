4f292745e51ff1c12065e4296076633c9d5788c8
clement4saunier <clement4saunier@gmail.com>
2023-05-18 17:35:45 +0200

    [Update] wallet connectivity

--- frontend/src/components/pages/Transfer.tsx
+++ frontend/src/components/pages/Transfer.tsx
@@ -2,11 +2,12 @@ import type { Component } from 'solid-js';
 import useWallet from '../../hooks/useWallet';
 
 const Transfer: Component = () => {
-  const {account} = useWallet();
+  const {account, connect} = useWallet();
 
   return (
     <div>
       <h1>Transfer</h1>
+      <button onClick={connect}>Connect</button>
       <p>
         {account()}
       </p>
--- frontend/src/hooks/useWallet.ts
+++ frontend/src/hooks/useWallet.ts
@@ -1,19 +1,48 @@
 import type { Accessor } from "solid-js";
 import { createSignal, createRenderEffect, onMount, onCleanup } from "solid-js";
-import { BigNumber, Provider } from "ethers";
+import { BigNumber, BrowserProvider, Provider, ethers} from "ethers";
 
 function useWallet(): {
   chainId: Accessor<number | undefined>;
   account: Accessor<string>;
-  provider: Accessor<Provider | undefined>;
+  provider: Accessor<BrowserProvider | unknown>;
+  connect: Function;
 } {
   const [connected, setConnected] = createSignal(false);
   const [chainId, setChainId] = createSignal(undefined);
   const [account, setAccount] = createSignal("");
-  const [provider, setProvider] = createSignal(undefined);
+  const [provider, setProvider] = createSignal();
 
-  function onChainChange(chainId) {
+  async function connectBrowserWallet(askSigner: boolean = true) {
+    const _provider = new BrowserProvider(window.ethereum, "any");
+    
+    if (!_provider) return;
+    
+    setProvider(_provider);
+    console.log(`🖧 Connected to network ${(await _provider.getNetwork()).chainId}`);
+
+    if (!(await _provider.hasSigner(0)) && !askSigner) return;
+
+    try {
+      const signer = await _provider.getSigner();
+      const address = signer.address;
+
+      setConnected(true);
+      setAccount(address);
+      console.log(`🦊 Browser Wallet connected as ${address}`);
+    } catch (_) {
+      console.log("🦊 Browser Wallet not connected");
+      setConnected(false);
+    }
+
+  }
+
+  connectBrowserWallet(false)
+
+  function onChainChange(chainId: string) {
     const newChainId = parseInt(chainId);
+
+    console.log("🖧 Switched to network", newChainId);
   }
 
   function onAccountChange(accounts: string[]) {
@@ -24,7 +53,7 @@ function useWallet(): {
     } else {
       setConnected(true);
       setAccount(accounts[0]);
-      console.log("👤 Connected as", accounts[0]);
+      console.log(`👤 Connected as ${accounts[0]}`);
     }
   }
 
@@ -40,7 +69,7 @@ function useWallet(): {
     window.ethereum.removeListener("accountsChanged", onAccountChange);
   });
 
-  return { chainId, account, provider };
+  return { chainId, account, provider, connect: connectBrowserWallet };
 }
 
 export default useWallet;
