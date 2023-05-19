1ba48740a7c421bdd86a020744b04202caf5b9b9
clement4saunier <clement4saunier@gmail.com>
2023-05-19 23:09:13 +0200

    [Add] contract deployment

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+// task("node", "Starts hardhat node and initliazes contracts for local testing", async (args, hre, runSuper) => {
+//   runSuper(args, hre);
+  
+//   console.log("ran");
+  
+// });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
